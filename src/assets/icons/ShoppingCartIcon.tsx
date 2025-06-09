401d3a45df0e50b07c14f405c8d530d44de1684c
Mazen Afifi <mazen.afifi@flairstech.com>
2025-06-10 00:38:03 +0300

    Sidebar navigation task done successfully

---
+++ src/assets/icons/ShoppingCartIcon.tsx
@@ -0,0 +1,9 @@
+const ShoppingCartIcon: React.FC<React.SVGProps<SVGSVGElement>> = () => (
+  <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2 2.29639H3.74001C4.82001 2.29639 5.67 3.22639 5.58 4.29639L4.75 14.2564C4.61 15.8864 5.89999 17.2864 7.53999 17.2864H18.19C19.63 17.2864 20.89 16.1064 21 14.6764L21.54 7.17639C21.66 5.51639 20.4 4.16638 18.73 4.16638H5.82001" stroke="#333333" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M16.25 22.2964C16.9404 22.2964 17.5 21.7367 17.5 21.0464C17.5 20.356 16.9404 19.7964 16.25 19.7964C15.5596 19.7964 15 20.356 15 21.0464C15 21.7367 15.5596 22.2964 16.25 22.2964Z" stroke="#333333" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M8.25 22.2964C8.94036 22.2964 9.5 21.7367 9.5 21.0464C9.5 20.356 8.94036 19.7964 8.25 19.7964C7.55964 19.7964 7 20.356 7 21.0464C7 21.7367 7.55964 22.2964 8.25 22.2964Z" stroke="#333333" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M9 8.29639H21" stroke="#333333" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+  </svg>
+);
+export default ShoppingCartIcon;
