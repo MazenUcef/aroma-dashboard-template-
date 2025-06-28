43a198a3ce4dba772f6b0a37c276c8e9e6f8044d
Mazen Afifi <mazen.afifi@flairstech.com>
2025-06-29 00:54:03 +0300

    Add / Edit Product done successfully

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { useState } from "react";
+import ProductModal from "../components/ProductModal";
 
 const Products = () => {
-  return <div>Products</div>;
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [isEditing] = useState(false);
+  const [selectedCategory] = useState<any>(null);
+
+  return (
+    <div>
+      <ProductModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onSubmit={(data) => {
+          console.log(data);
+          setIsModalOpen(false);
+        }}
+        isEditMode={isEditing}
+        initialData={isEditing ? selectedCategory : undefined}
+      />
+    </div>
+  );
 };
 
 export default Products;
