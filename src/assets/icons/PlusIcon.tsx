8817333c02b5c9502cbbfb80aa457fcf985f589e
Mazen Afifi <mazen.afifi@flairstech.com>
2025-07-20 12:46:50 +0300

    .

--- src/assets/icons/PlusIcon.tsx
+++ src/assets/icons/PlusIcon.tsx
@@ -1,6 +1,4 @@
-
-
-const PlusIcon = () => {
+const PlusIcon = ({ fill = "#111928", stroke = "#1F2A37" }) => {
   return (
     <div>
       <svg
@@ -10,10 +8,10 @@ const PlusIcon = () => {
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path d="M8.33464 8.55046H3.66797H8.33464Z" fill="#111928" />
+        <path d="M8.33464 8.55046H3.66797H8.33464Z" fill={fill} />
         <path
           d="M8.33464 3.88379V8.55046M8.33464 8.55046V13.2171M8.33464 8.55046H13.0013M8.33464 8.55046H3.66797"
-          stroke="currentColor"
+          stroke={stroke}
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
